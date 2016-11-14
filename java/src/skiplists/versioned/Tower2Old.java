560db904ba7f793e247a718aa1fb68886e6af329
Tyler Crain <tyler.crain@lip6.fr>
2016-11-14 19:20:36 +1100

    trying different tower configurations

---
+++ java/src/skiplists/versioned/Tower2Old.java
@@ -0,0 +1,86 @@
+package skiplists.versioned;
+
+import java.lang.reflect.Field;
+
+import sun.misc.Unsafe;
+
+public class Tower2Old {
+    public final int val;
+
+    public final Tower2Old[] nexts;
+
+    public volatile int height = 0;
+
+    /* 0 = being inserted, 1 = valid, 2 = being deleted */
+    public volatile int status = 0;
+
+    private volatile int lock;
+
+    private static final int VERSION_BIT_MASK = -2;
+
+    public Tower2Old(int val, int maxHeight, int height) {
+        this.val = val;
+        this.nexts = new Tower2Old[height];
+        this.lock = 0;
+        this.height = height;
+    }
+    
+    // UNSAFE mechanics
+
+    private static final sun.misc.Unsafe UNSAFE;
+    private static final long lockOffset;
+
+    static {
+        try {
+            UNSAFE = getUnsafe();
+            Class<?> k = Tower2Old.class;
+            lockOffset = UNSAFE.objectFieldOffset
+                (k.getDeclaredField("lock"));
+        } catch (Exception e) {
+            throw new Error(e);
+        }
+    }
+    
+    @SuppressWarnings("restriction")
+    private static Unsafe getUnsafe() {
+        try {
+
+        	Field f = Unsafe.class.getDeclaredField("theUnsafe");
+        	f.setAccessible(true);
+        	Unsafe unsafe = (Unsafe) f.get(null);
+        	return unsafe;
+
+        } catch (Exception e) {
+        	System.err.println("error getting unsafe");
+        	return null;
+        }
+    }
+    
+
+    public int getVersion() {
+        return (lock & VERSION_BIT_MASK);
+    }
+
+    public boolean tryLockAtVersion(int version) {
+    	return UNSAFE.compareAndSwapInt(this, lockOffset, version, version +1);
+    }
+
+    public void spinlock() {
+        int version = getVersion();
+        while (!UNSAFE.compareAndSwapInt(this, lockOffset, version, version + 1)) {
+            version = getVersion();
+        }
+    }
+
+    public void unlockAndIncrementVersion() {
+        lock = lock + 1;
+    }
+
+    public void unlock() {
+        lock = lock - 1;
+    }
+
+    public void resetLocks() {
+        lock = 0;
+    }
+}
---
+++ java/src/skiplists/versioned/Tower2Old2.java
@@ -0,0 +1,57 @@
+package skiplists.versioned;
+
+import java.util.concurrent.atomic.AtomicIntegerFieldUpdater;
+
+public class Tower2Old2 {
+    public final int val;
+
+    public final Tower2Old2[] nexts;
+
+    public volatile int height = 0;
+
+    /* 0 = being inserted, 1 = valid, 2 = being deleted */
+    public volatile int status = 0;
+
+    private volatile int lock;
+    
+    private final static AtomicIntegerFieldUpdater<Tower2Old2>
+    	atomicIntegerFieldUpdater = AtomicIntegerFieldUpdater.newUpdater(
+            Tower2Old2.class, "lock" );
+
+    private static final int VERSION_BIT_MASK = -2;
+
+    public Tower2Old2(int val, int maxHeight, int height) {
+        this.val = val;
+        this.nexts = new Tower2Old2[height];
+        atomicIntegerFieldUpdater.set(this, 0);
+        this.height = height;
+    }
+    
+
+    public int getVersion() {
+        return (lock & VERSION_BIT_MASK);
+    }
+
+    public boolean tryLockAtVersion(int version) {
+    	return atomicIntegerFieldUpdater.compareAndSet(this, version, version +1);
+    }
+
+    public void spinlock() {
+        int version = getVersion();
+        while (!atomicIntegerFieldUpdater.compareAndSet(this, version, version + 1)) {
+            version = getVersion();
+        }
+    }
+
+    public void unlockAndIncrementVersion() {
+    	atomicIntegerFieldUpdater.set(this, atomicIntegerFieldUpdater.get(this) + 1);
+    }
+
+    public void unlock() {
+    	atomicIntegerFieldUpdater.set(this, atomicIntegerFieldUpdater.get(this) - 1);
+    }
+
+    public void resetLocks() {
+    	atomicIntegerFieldUpdater.set(this, 0);
+    }
+}
